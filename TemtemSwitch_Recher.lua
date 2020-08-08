9e470fd6723174071477c115b577a0be55c4a29b
tyboe2013 <40647812+tyboe2013@users.noreply.github.com>
2020-08-08 19:21:09 -0400

    Add files via upload

---
+++ TemtemSwitch_Recher.lua
@@ -0,0 +1,28 @@
+import ('TemBot.Lua.TemBotLua')
+
+--Registering the Temtem Window
+tblua:RegisterTemTemWindow()
+
+tblua:Sleep(1500)
+
+if tblua:IsInWorld() == true then
+ while(true)
+ do
+    tblua:Sleep(100)
+    --check pause and logout user settings
+    tblua:CheckPause()
+    tblua:CheckLogout()
+    if tblua:IsInFight() == true then
+        tblua:Sleep(500)
+        tblua:PressKey(0x35)
+        tblua:Sleep(500)
+        tblua:PressKey(0x53)
+        tblua:Sleep(500)
+        tblua:PressKey(0x46)
+        tblua:Sleep(25000)
+    end
+
+end
+else
+tblua:TestMessage('Error: Not ready to start the script')
+end
